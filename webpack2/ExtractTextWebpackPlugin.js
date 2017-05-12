f2d515b3c0f10b6e2bc9fc43f51435b3b8958a04
anonymous <xgqfrms@gmail.com>
2017-05-12 23:37:14 +0800

    Update ExtractTextWebpackPlugin.js

--- webpack2/ExtractTextWebpackPlugin.js
+++ webpack2/ExtractTextWebpackPlugin.js
@@ -47,6 +47,46 @@ module.exports = {
 
 
 
+// new
+
+
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: ['css-loader', 'sass-loader'],
+                publicPath: "/dist"
+            })
+        }]
+    },
+    devtool: "source-map",
+    plugins: [
+        new ExtractTextPlugin({
+            filename: "bundle.css",
+            disable: false,
+            allChunks: true
+        }),
+        new UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: true
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        }),
+        new webpack.BannerPlugin({
+            banner: 'Banner', 
+            raw: true, 
+            entryOnly: true
+        })
+    ]
+}
 
 
 
